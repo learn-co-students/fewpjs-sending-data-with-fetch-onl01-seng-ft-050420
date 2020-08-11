a9001007359fc1801df826ef211904da7960cdfa
matthewlazewski <matthew.lazewski@gmail.com>
2020-08-10 21:11:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+   
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+   
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+
+
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let h1 = document.createElement("h1")
+        h1.innerHTML = object.id;
+        document.body.appendChild(h1);
+        console.log(object);
+    })
+    .catch(function(error) { 
+        let p = document.createElement("p")
+        p.innerHTML = error.message;
+        document.body.appendChild(p);
+        console.log(error.message);
+    });
+};
+
+
