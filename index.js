dd1f34d2318f9760efc9558d4c012c76b19f68ba
aaron-schuyler <asky28@gmail.com>
2020-08-12 15:09:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const body = document.body
+
+function submitData(name, email) {
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: name, email: email})
+  }
+return fetch('http://localhost:3000/users', config)
+  .then(res => res.json())
+  .then(json => addToBody(json.id))
+  .catch(err => addToBody(err.message))
+}
+
+function addToBody(text) {
+  body.appendChild(document.createTextNode(text))
+}
