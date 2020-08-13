bcd49bb2ef8f69a78498fe587e3334693e073015
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-12 22:57:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    };
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(object => {
+        const h1 = document.createElement('h1')
+        h1.textContent = object.id
+        document.body.appendChild(h1)
+    })
+    .catch(error => {
+        const h2 = document.createElement('h2')
+        h2.textContent = error.message
+        document.body.appendChild(h2)
+    });
+}
