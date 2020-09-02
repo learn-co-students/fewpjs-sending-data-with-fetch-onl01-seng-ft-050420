3b48b22fb0e56e7539dda8a839e562b1b968090c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-09-02 03:35:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: name, email: email})
+  };
+
+return fetch("http://localhost:3000/users", configObj)
+  .then(res => res.json())
+  .then(object => {
+      const h1 = document.createElement('h1')
+      h1.textContent = object.id
+      document.body.appendChild(h1)
+  })
+  .catch(error => {
+      const h2 = document.createElement('h2')
+      h2.textContent = error.message
+      document.body.appendChild(h2)
+  });
+}
